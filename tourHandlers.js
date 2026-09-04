a1621cb4cfc3bf49ead333579090bc6a4ae84a3f
Sam-P96 <samnap7@gmail.com>
2026-09-04 15:01:11 +0300

    Complete Iter 0

---
+++ tourHandlers.js
@@ -0,0 +1,114 @@
+// const Tours = require("./ToursLib");
+
+// const getAllTours = (req, res) => {
+//   // const tours = Tours.getAll();
+//   res.json({ message: "Hello from getAllTours" });
+//   // res.json(tours);
+// };
+
+
+const getAllTours = (req, res) => {
+  // const tours = Tours.getAll();
+  res.json({ message: "Hello from 1s" });
+  // res.json(tours);
+};
+
+
+// REMEMBER TO FIX THESE
+const createTour = (req, res) => {
+  res.json({ message: "Hello from 2" });
+};
+  
+
+// Remember to double check
+const getTourById = (req, res) => {
+  res.json({ message: "Hello from 3" });
+};
+
+const updateTour = (req, res) => {
+  cres.json({ message: "Hello from 4s" });
+};
+
+const deleteTour = (req, res) => {
+  res.json({ message: "Hello from 5" });
+};
+
+module.exports = {
+    getAllTours,
+    createTour,
+    getTourById,
+    updateTour,
+    deleteTour
+}
+
+
+
+// // REMEMBER TO FIX THESE
+// const createTour = (req, res) => {
+//   const { sender, message, rating, platform } = req.body;
+//   if (!sender || !message || !platform || rating < 1 || rating > 5) {
+//     return res.status(400).json({
+//     message: "Invalid feedback data",
+//   })} else {
+//      res.json({ message: "Hello from createTour"}
+//     // const newTour = Tour.addOne(
+//     // sender,
+//     // message,
+//     // rating,
+//     // platform
+//   );
+//   if (newTour) {
+//     res.status(201).json(newTour);
+//   } else {
+//     res.status(500).json({ message: "Failed to create Tour" });
+//   }
+// }};
+  
+
+// // Remember to double check
+// const getTourById = (req, res) => {
+//   const tourId = req.params.tourId;
+//   const tour = Tours.findById(tourId);
+//   if (tour) {
+//     res.json(tour);
+//   } else {
+//     res.status(404).json({ message: "Tour not found" });
+//   }
+//   res.json({ message: "Hello from getTourkById" });
+// };
+
+// const updateTour = (req, res) => {
+//   const tourId = req.params.tourId;
+
+//   const updatedTour = Tour.update(
+//     tourId,
+//     req.body
+//   );
+
+//   if (updatedTour) {
+//      res.json({ message: "Hello from updateTour"})
+//     // res.json(updatedTour);
+//   } else {
+//     res.status(404).json({ message: "Tour not found" });
+//   }
+// };
+
+// const deleteTour = (req, res) => {
+//   const tourId = req.params.tourId;
+
+//   const isDeleted = Tour.deleteOne(tourkId);
+
+//   if (isDeleted) {
+//     res.status(204).send();
+//   } else {
+//     res.status(404).json({ message: "Tour not found" });
+//   }
+// };
+
+// module.exports = {
+//     getAllTours,
+//     createTour,
+//     getTourById,
+//     updateTour,
+//     deleteTour
+// }
