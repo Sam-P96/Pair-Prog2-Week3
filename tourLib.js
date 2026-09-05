da6a90e23fa8a55f1343fded849cac057d759194
Ngan Tran <tranlebaongan1811@gmal.com>
2026-09-05 13:39:55 +0300

    list all tours

--- tourLib.js
+++ tourLib.js
@@ -18,3 +18,27 @@ if (require.main === module) {
 
   console.log(result);
 }
+
+const getAll = () => {
+  return Tours;
+};
+
+if (require.main === module) {
+  addOne(
+    "Paris in 7 Days",
+    "Explore Paris.",
+    "https://example.com/paris.jpg",
+    "1,995",
+    "Paris, France"
+  );
+
+  addOne(
+    "Helsinki Weekend",
+    "Discover Helsinki.",
+    "https://example.com/helsinki.jpg",
+    "795",
+    "Helsinki, Finland"
+  );
+
+  console.log(getAll());
+}
